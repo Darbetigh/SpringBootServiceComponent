b992f7c76326f70a28e8668dbba2291c2e522517
Darbetigh <mustafaakgul@yandex.com>
2019-02-14 18:09:39 +0300

    Initial commit

---
+++ src/main/java/com/example/servicecomponent/ServicecomponentApplication.java
@@ -0,0 +1,14 @@
+package com.example.servicecomponent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicecomponentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicecomponentApplication.class, args);
+    }
+
+}
+
